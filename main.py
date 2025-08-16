2881466a1c037b3475b4a67087d23e0642a66509
Davearnoldnatio <davearnoldnation@gmail.com>
2025-08-16 03:04:31 -0500

    Not quite there yet, issues with the initial for loop and sort function

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ def main():
 
     from stats import get_word_count
     from stats import get_character_count
+    from stats import get_sorted_char_count
 
     book_text = get_book_text(book)
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
-
+    sorted_character_count = get_sorted_char_count(character_count)
     #word_count function taking get_book_text() as an argument
     print(f"{word_count} words found in the document")
     print(character_count)
-    
+    print(sorted_character_count)
+
 main()
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+""" The stats functions"""
+
+
 def get_word_count(book_text):
     word_list = book_text.split()
     #gives a list of the words
@@ -13,4 +16,12 @@ def get_character_count(book_text):
             character_count[word] = 1
     return character_count
         
-        
+def get_sorted_char_count(character_count):
+    list_of_dict = []
+
+    for key, value in character_count.items() and character_count.values().isalpha():
+        list_of_dict.append({key: value})
+
+    for character in list_of_dict:
+        sorted_character_count = list_of_dict.sort(reverse=True, key = lambda a: list(a.items())[0][1])
+    return sorted_character_count
